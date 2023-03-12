00ab469905685ce03da5892081cb426f682c37c1
urfaty <97344694+urfaty@users.noreply.github.com>
2023-03-13 02:07:13 +0300

    Add files via upload
    frontend backend veri alışverişi eklendi

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name = "blog-home"),
     path("about/", views.about, name = "blog-about"),
-
+    path('submit_data/', views.submit_data_view, name='submit_data'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,3 +6,18 @@ def home(request):
 
 def about(request):
 	return render(request, "blog/about.html")
+
+def submit_data_view(request):
+    if request.method == 'POST':
+        my_data = request.POST.get('my_data')
+
+        # The input text is input_field.
+        # we can send output through context to fronted. 
+
+        context = {
+        'message': my_data
+        }
+
+        return render(request, 'blog/home.html', context)
+    else:
+        return render(request, 'blog/about.html')
